5201de35b573be2cb827fb56a6f58142a6f702ee
ZenithGD <darimarcos456@hotmail.com>
2022-12-20 21:56:43 +0100

    fix actions JVM version

--- build.gradle.kts
+++ build.gradle.kts
@@ -51,7 +51,7 @@ subprojects {
     tasks.withType<KotlinCompile> {
         kotlinOptions {
             freeCompilerArgs = listOf("-Xjsr305=strict")
-            jvmTarget = "11"
+            jvmTarget = "17"
         }
     }
 
